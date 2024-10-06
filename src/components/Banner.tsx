4421f6459211b337152a5d05bcf1623c8bcea42e
Zain <m.zainhassanbaloch@gmail.com>
2024-10-07 03:59:49 +0500

    Banner initial component added

---
+++ src/components/Banner.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import parrotImg from '@/public/images/parrot.png';
+
+const Banner = () => {
+  return (
+    <div className='w-full'>
+
+    </div>
+  )
+}
+
+export default Banner
